d2d97e97356a111278c6277dc3ef0f9f94aa114c
sshende-java <80737493+sshende-java@users.noreply.github.com>
2025-08-29 15:26:09 +0530

    Redis-RateLimiter at Gateway Level

--- gateway/src/main/java/com/ecommerce/gateway/GatewayConfig.java
+++ gateway/src/main/java/com/ecommerce/gateway/GatewayConfig.java
@@ -1,21 +1,42 @@
 package com.ecommerce.gateway;
 
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
 
 //Java based Routing config instead of yml , beneficial for adding filters or modifying request itself
 @Configuration
 public class GatewayConfig {
 
     @Bean
+    public RedisRateLimiter redisRateLimiter() {
+        //explanation below table
+        return new RedisRateLimiter(10, 20, 1);
+    }
+
+    //KeyResolver is an interface used by Spring Cloud Gateway to resolve a key for rate limiting.
+    //rate limiting based on user hostname
+    @Bean
+    public KeyResolver hostNameKeyResolver() {
+        return exchange -> Mono.just(
+                exchange.getRequest().getRemoteAddress().getHostName()
+        );
+    }
+
+
+    @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("product-microservice", r -> r.path("/api/products/**")
-                        .filters(f->f.circuitBreaker(config -> config.setName("ecomCircuitBreaker")
-                                                    .setFallbackUri("forward:/fallback/products")
-                                                    ).retry(retryConfig -> retryConfig.setRetries(5)))  //f->f.circuitBreaker().retry()     like this
+                        .filters(f -> f.circuitBreaker(config -> config.setName("ecomCircuitBreaker")
+                                .setFallbackUri("forward:/fallback/products")
+                        ).retry(retryConfig -> retryConfig.setRetries(5))
+                         .requestRateLimiter(rl-> rl.setRateLimiter(redisRateLimiter()).setKeyResolver(hostNameKeyResolver()))
+                        )  //f->f.circuitBreaker().retry().requestRateLimiter()    like this
                         .uri("lb://PRODUCT-MICROSERVICE"))
                 .route("user-microservice", r -> r.path("/api/users/**").uri("lb://USER-MICROSERVICE"))
                 .route("order-microservice", r -> r.path("/api/orders/**", "/api/cart/**").uri("lb://ORDER-MICROSERVICE"))
@@ -26,3 +47,9 @@ public class GatewayConfig {
                 .build();
     }
 }
+
+
+//Parameter	        Value	    Meaning
+//replenishRate	    10	        Number of tokens to add per second (i.e. 10 requests/second allowed)
+//burstCapacity	    20	        Max number of tokens that can be stored (i.e. burst up to 20 requests instantly)
+//requestedTokens	1	        Number of tokens required per request (default is usually 1)
