1fbef79d75943a6296400a79498e7749babef6e4
sshende-java <80737493+sshende-java@users.noreply.github.com>
2025-08-29 11:58:13 +0530

    RateLimiter in Order-Service

--- order-microservice/src/main/java/com/ecommerce/order/controller/MessageController.java
+++ order-microservice/src/main/java/com/ecommerce/order/controller/MessageController.java
@@ -1,9 +1,14 @@
 package com.ecommerce.order.controller;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+//@RefreshScope bean will be re-initialized, will pick up updated configuration values
+//You can trigger a refresh by calling the /actuator/refresh on any service-endpoint as you ALL service are connected through spring-cloud-bus and rabbitMQ
+//RabbitMQ will broadcast refresh event to all services
 
 @RestController
 @RefreshScope
@@ -15,9 +20,13 @@ public class MessageController {
     @Value("${app.centralisedMessage:pqr}")
     private String centralisedMessage;
 
+    @RateLimiter(name="ratelimiterInstance1",fallbackMethod = "getMessageRateLimitFallback")
     @GetMapping("/message")
     public String getMessage() {
         return message+" "+centralisedMessage;
     }
 
+    public String getMessageRateLimitFallback(Exception e) {
+        return "Hello Fallback!";
+    }
 }
