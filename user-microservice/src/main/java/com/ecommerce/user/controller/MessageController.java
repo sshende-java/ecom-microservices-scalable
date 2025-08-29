73af27610f507531f79fc8f1672e1fabfc31f128
sshende-java <80737493+sshende-java@users.noreply.github.com>
2025-08-29 11:57:38 +0530

    Comments- @RefreshScope

--- user-microservice/src/main/java/com/ecommerce/user/controller/MessageController.java
+++ user-microservice/src/main/java/com/ecommerce/user/controller/MessageController.java
@@ -5,6 +5,10 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//@RefreshScope bean will be re-initialized, will pick up updated configuration values
+//You can trigger a refresh by calling the /actuator/refresh on any service-endpoint as you ALL service are connected through spring-cloud-bus and rabbitMQ
+//RabbitMQ will broadcast refresh event to all services
+
 @RestController
 @RefreshScope
 public class MessageController {
